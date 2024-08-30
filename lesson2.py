49905948488d01f0a681a266be59b1cd74bf65f0
Remiasz <dremiasz@cps.k12.il.us>
2024-08-30 15:06:29 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,40 @@
+# print("Hello world")
+# day_of_week = input ("What day of the week is it?")
+# print ("Heppy " + day_of_week)
+#concantination is when you add to strings together
+#using a plus (+)
+# movies_this_week = input("What are you watching this week? ")
+# print("I am watching, " + movies_this_week + " ,this week")
+# mood = input("How are you feeling today?")
+# print("I am feeling " + mood)
+
+# #data types for variables in python
+# #strings - text
+# Name = "Dominik" # this is a string data type
+# #whever it is wrapped in quotes it is a string
+# #integers - whole numbers
+# year = 2024 # this is an integer data type
+# # does not need to be wrapped in quotes
+# yearfourfromnow = 2028
+# subtract = yearfourfromnow - year
+# print(subtract)
+# #floats - decimal numbers
+# pricebigmac = 3.99 # this is a flow data type
+# pricedoublepounder = 4.99
+# totalprice = pricebigmac + pricedoublepounder
+# print(totalprice)
+# #booleans - True or False
+# itsraining = False # this is a boolean data type
+# print(itsraining)
+# #lists - a collection of items
+# grociers = ["apples", "bananas", "strawberries"]
+# print(grociers)
+# int makes astatement into an integer
+# challenge 1
+movies = ["Deadpool and Wolvirine" + "BeetleJuiceBeedleJuice" + "Alien: Romulus"]
+restaurantaddress = "7-11"
+restauranttime = 9.00
+movietime = 7.00
+movieprice = 5
+peoplegoing = 4
+print("We are going tyo see" + movies + "at" + movietime + "then go to dinner at" + restauranttime + "at" + restaurantaddress + "with" + peoplegoing + "people")
